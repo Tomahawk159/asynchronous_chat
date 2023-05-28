ae3d98361e40527cedc438115e64420ecddac31e
Viktor <tomahabk59@gmail.com>
2023-05-28 14:33:35 +0500

    DZ_13

--- client.py
+++ client.py
@@ -1,157 +1,21 @@
-import sys
-import json
-import socket
-import time
-import argparse
 import logging
-import threading
+import argparse
+import sys
+from PyQt5.QtWidgets import QApplication
+
 from common.variables import *
-from common.utils import *
-from errors import IncorrectDataRecivedError, ReqFieldMissingError, ServerError
+from common.errors import ServerError
+from common.decos import log
+from client.database import ClientDatabase
+from client.transport import ClientTransport
+from client.main_window import ClientMainWindow
+from client.start_dialog import UserNameDialog
 
-from decorators import log
-from metaclasses import ClientMaker
-
-from db_client import StorageClient
-
+# Инициализация клиентского логера
 logger = logging.getLogger("client")
-sock_lock = threading.Lock()
-database_lock = threading.Lock()
 
 
-class ClientSender(threading.Thread, metaclass=ClientMaker):
-    def __init__(self, account_name, sock, database):
-        self.account_name = account_name
-        self.sock = sock
-        self.database = database
-        super().__init__()
-
-    def create_exit_message(self):
-        return {ACTION: EXIT, TIME: time.time(), ACCOUNT_NAME: self.account_name}
-
-    def create_message(self):
-        to = input("Введите получателя сообщения: ")
-        message = input("Введите сообщение для отправки: ")
-        message_dict = {
-            ACTION: MESSAGE,
-            SENDER: self.account_name,
-            "to": to,
-            TIME: time.time(),
-            MESSAGE_TEXT: message,
-        }
-        logger.debug(f"Сформирован словарь сообщения: {message_dict}")
-        with database_lock:
-            self.database.save_message(self.account_name, to, message)
-
-        with sock_lock:
-            try:
-                send_message(self.sock, message_dict)
-                logger.info(f"Отправлено сообщение для пользователя {to}")
-            except:
-                logger.critical("Потеряно соединение с сервером.")
-                exit(1)
-
-    def run(self):
-        self.print_help()
-        while True:
-            command = input("Введите команду: ")
-            if command == "message":
-                self.create_message()
-            elif command == "help":
-                self.print_help()
-            elif command == "exit":
-                with sock_lock:
-                    try:
-                        send_message(self.sock, self.create_exit_message())
-                    except:
-                        pass
-                    print("Завершение соединения.")
-                logger.info("Завершение работы по команде пользователя.")
-                time.sleep(0.5)
-                break
-            else:
-                print(
-                    "Команда не распознана, попробойте снова. help - вывести поддерживаемые команды."
-                )
-
-    def print_help(self):
-        print("Поддерживаемые команды:")
-        print("message - отправить сообщение. Кому и текст будет запрошены отдельно.")
-        print("help - вывести подсказки по командам")
-        print("exit - выход из программы")
-
-
-class ClientReader(threading.Thread, metaclass=ClientMaker):
-    def __init__(self, account_name, sock, database):
-        self.account_name = account_name
-        self.sock = sock
-        self.database = database
-        super().__init__()
-
-    def run(self):
-        while True:
-            time.sleep(1)
-            with sock_lock:
-                try:
-                    message = get_message(self.sock)
-                    if (
-                        ACTION in message
-                        and message[ACTION] == MESSAGE
-                        and SENDER in message
-                        and "to" in message
-                        and MESSAGE_TEXT in message
-                        and message["to"] == self.account_name
-                    ):
-                        print(
-                            f"\nПолучено сообщение от пользователя {message[SENDER]}:\n{message[MESSAGE_TEXT]}"
-                        )
-                        with database_lock:
-                            try:
-                                self.database.save_message(
-                                    message[SENDER],
-                                    self.account_name,
-                                    message[MESSAGE_TEXT],
-                                )
-                            except:
-                                logger.error("Ошибка взаимодействия с базой данных")
-                        logger.info(
-                            f"Получено сообщение от пользователя {message[SENDER]}:\n{message[MESSAGE_TEXT]}"
-                        )
-                    else:
-                        logger.error(
-                            f"Получено некорректное сообщение с сервера: {message}"
-                        )
-                except IncorrectDataRecivedError:
-                    logger.error(f"Не удалось декодировать полученное сообщение.")
-                except (
-                    OSError,
-                    ConnectionError,
-                    ConnectionAbortedError,
-                    ConnectionResetError,
-                    json.JSONDecodeError,
-                ):
-                    logger.critical(f"Потеряно соединение с сервером.")
-                    break
-
-
-@log
-def create_presence(account_name):
-    out = {ACTION: PRESENCE, TIME: time.time(), USER: {ACCOUNT_NAME: account_name}}
-    logger.debug(f"Сформировано {PRESENCE} сообщение для пользователя {account_name}")
-    return out
-
-
-@log
-def process_response_ans(message):
-    logger.debug(f"Разбор приветственного сообщения от сервера: {message}")
-    if RESPONSE in message:
-        if message[RESPONSE] == 200:
-            return "200 : OK"
-        elif message[RESPONSE] == 400:
-            raise ServerError(f"400 : {message[ERROR]}")
-    raise ReqFieldMissingError(RESPONSE)
-
-
+# Парсер аргументов коммандной строки
 @log
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -172,60 +36,37 @@ def arg_parser():
     return server_address, server_port, client_name
 
 
-def main():
-    print("Консольный месседжер. Клиентский модуль.")
-
+if __name__ == "__main__":
     server_address, server_port, client_name = arg_parser()
 
+    client_app = QApplication(sys.argv)
+
     if not client_name:
-        client_name = input("Введите имя пользователя: ")
-    else:
-        print(f"Клиентский модуль запущен с именем: {client_name}")
+        start_dialog = UserNameDialog()
+        client_app.exec_()
+        if start_dialog.ok_pressed:
+            client_name = start_dialog.client_name.text()
+            del start_dialog
+        else:
+            exit(0)
 
     logger.info(
         f"Запущен клиент с парамертами: адрес сервера: {server_address} , порт: {server_port}, имя пользователя: {client_name}"
     )
 
+    database = ClientDatabase(client_name)
+
     try:
-        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        transport.connect((server_address, server_port))
-        send_message(transport, create_presence(client_name))
-        answer = process_response_ans(get_message(transport))
-        logger.info(f"Установлено соединение с сервером. Ответ сервера: {answer}")
-        print(f"Установлено соединение с сервером.")
-    except json.JSONDecodeError:
-        logger.error("Не удалось декодировать полученную Json строку.")
-        exit(1)
+        transport = ClientTransport(server_port, server_address, database, client_name)
     except ServerError as error:
-        logger.error(f"При установке соединения сервер вернул ошибку: {error.text}")
+        print(error.text)
         exit(1)
-    except ReqFieldMissingError as missing_error:
-        logger.error(
-            f"В ответе сервера отсутствует необходимое поле {missing_error.missing_field}"
-        )
-        exit(1)
-    except (ConnectionRefusedError, ConnectionError):
-        logger.critical(
-            f"Не удалось подключиться к серверу {server_address}:{server_port}, конечный компьютер отверг запрос на подключение."
-        )
-        exit(1)
-    else:
-        database = StorageClient(client_name)
+    transport.setDaemon(True)
+    transport.start()
 
-        module_sender = ClientSender(client_name, transport, database)
-        module_sender.daemon = True
-        module_sender.start()
-        logger.debug("Запущены процессы")
-        module_reciver = ClientReader(client_name, transport, database)
-        module_reciver.daemon = True
-        module_reciver.start()
-
-        while True:
-            time.sleep(1)
-            if module_reciver.is_alive() and module_sender.is_alive():
-                continue
-            break
-
-
-if __name__ == "__main__":
-    main()
+    main_window = ClientMainWindow(database, transport)
+    main_window.make_connection(transport)
+    main_window.setWindowTitle(f"Чат Программа alpha release - {client_name}")
+    client_app.exec_()
+    transport.transport_shutdown()
+    transport.join()
